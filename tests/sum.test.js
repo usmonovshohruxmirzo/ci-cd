86081144be5a684a401539b51d4b1cbaef332e0a
usmonovshohruxmirzo <usmonovshohruxmirzo@users.noreply.github.com>
2025-09-25 13:47:40 +0000

    deploy: cd0d89eb8193d263e5e59ec4cacfdd57698e8f7f

--- tests/sum.test.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function sum(a, b) {
-    return a + b;
-}
-test('adds 1 + 2 = 3', () => {
-    expect(sum(1, 2)).toBe(3);
-});
-//# sourceMappingURL=sum.test.js.map
