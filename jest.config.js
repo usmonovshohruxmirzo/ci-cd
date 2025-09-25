e1f032405df7bdac15da417267d98266fe172151
usmonovshohruxmirzo <usmonovshohruxmirzo@users.noreply.github.com>
2025-09-25 13:47:48 +0000

    deploy: cd0d89eb8193d263e5e59ec4cacfdd57698e8f7f

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-undef */
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const { createDefaultPreset } = require("ts-jest");
+
+const tsJestTransformCfg = createDefaultPreset().transform;
+
+/** @type {import("jest").Config} **/
+module.exports = {
+  testEnvironment: "node",
+  transform: {
+    ...tsJestTransformCfg,
+  },
+};
